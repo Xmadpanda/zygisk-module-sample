4c27a02b865e3871002a048d280b90fda7dea02a
franzydx <dapzfranzy@gmail.com>
2025-03-25 01:24:10 +0700

    Update Projects
    Changes :
    - Update gradle version
    - Used cmake
    - Change to kotlin dsl

---
+++ module/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.android.lib)
+}
+
+android {
+    namespace  = "module.dpzdev.zygisk"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 35
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+            }
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
