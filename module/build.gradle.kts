2f86e6599245c0542ab19de0853814c6e706bb2a
Xmadpanda <159286513+Xmadpanda@users.noreply.github.com>
2025-03-26 21:44:20 +0700

    Update build.gradle.kts

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -25,29 +25,47 @@ android {
     }
 }
 
-afterEvaluate() {
+afterEvaluate {
     android.libraryVariants.forEach { variant ->
         val buildType: String = variant.name.replaceFirstChar { it.uppercase() }
+
+        // Task untuk menyalin file
         tasks.register<Copy>("copyLibs$buildType") {
             dependsOn("assemble${buildType}")
             println("Task : Module $buildType")
 
-            val trg = file("${project.layout.projectDirectory}/magisk/libs")
-            val src = file("${project.layout.buildDirectory.get()}/intermediates/stripped_native_libs" +
-                    "/${buildType.lowercase()}/strip${buildType}DebugSymbols/out/lib")
-            if (src.exists()) {
-                from(src)
-/*
-module/build/intermediates/stripped_native_libs/$type/strip$TypeDebugSymbols/out/lib/$type
-*/
-                into(trg)
-            } else {
-                println("File tidak ditemukan : ${src.toString()}")
-            }
+            val archTypes = listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
+            val srcBase = file("${project.layout.buildDirectory.get()}/intermediates/stripped_native_libs/${buildType.lowercase()}/strip${buildType}DebugSymbols/out/lib")
 
+            archTypes.forEach { archType ->
+                val srcFile = file("$srcBase/$archType/libSample.so")
+                val trgDir = file("${project.layout.projectDirectory}/magisk/zygisk/$archType")
+                val trgFile = file("$trgDir/$archType.so")
+
+                if (srcFile.exists()) {
+                    from(srcFile) {
+                        into(trgDir)
+                        rename { archType + ".so" } // Rename file sesuai arsitektur
+                    }
+                } else {
+                    println("File tidak ditemukan : ${srcFile.toString()}")
+                }
+            }
         }
 
         variant.assembleProvider.get().finalizedBy("copyLibs$buildType")
 
+        // Task untuk mengompres folder
+        tasks.register<Zip>("compressMagisk") {
+            dependsOn("copyLibs$buildType")
+            from(file("${project.layout.projectDirectory}/magisk")) {
+                include("**/*") // Menyertakan semua file dalam folder magisk
+            }
+            destinationDirectory.set(file("${project.layout.projectDirectory}/magisk"))
+            archiveFileName.set("magisk.zip") // Nama file zip
+        }
+
+        // Menjalankan tugas kompres setelah menyalin libs
+        tasks.named("copyLibs$buildType").get().finalizedBy("compressMagisk")
     }
-}
+} 
